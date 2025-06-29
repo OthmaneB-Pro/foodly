cf7471868a7ceb8c05c64ca23ea6d8895abc29fd
OthmaneB-Pro <othmane93.baar@gmail.com>
2025-06-29 11:09:20 +0200

    ♻️ F01 : login > create LoginForm

---
+++ src/components/pages/login/LoginForm.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+
+export default function LoginForm() {
+   const [username, setUsername] = useState("");
+  
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      alert(`Bonjour  ${username}`);
+      setUsername("");
+    };
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setUsername(event.target.value);
+    };
+    return (
+      <form onSubmit={handleSubmit}>
+        <h2>Bienvenue chez nous !</h2>
+        <h3>Connectez-vous</h3>
+  
+        <input
+          value={username}
+          onChange={handleChange}
+          placeholder="Entrez votre prénom"
+          type="text"
+          required
+        />
+        <button>Accédez à votre espace</button>
+      </form>
+    );
+}
--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -1,29 +1,9 @@
-import { useState } from "react";
+import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
-  const [username, setUsername] = useState("");
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    alert(`Bonjour  ${username}`);
-    setUsername("");
-  };
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value);
-  };
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Bienvenue chez nous !</h2>
-      <h3>Connectez-vous</h3>
-
-      <input
-        value={username}
-        onChange={handleChange}
-        placeholder="Entrez votre prénom"
-        type="text"
-        required
-      />
-      <button>Accédez à votre espace</button>
-    </form>
+    <div>
+      <LoginForm />
+    </div>
   );
 }
