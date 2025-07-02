cd453db410f0d8a70be5eec6ae04839d14099230
OthmaneB-Pro <othmane93.baar@gmail.com>
2025-07-02 13:28:34 +0200

    🚚 F04 : Logo > Rename LoginLogo to Logo and move to reusable components

--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import LoginForm from "./LoginForm";
-import LoginLogo from "./LoginLogo";
+import LoginLogo from "../../reusable-ui/Logo";
 
 export default function LoginPage() {
   return (
