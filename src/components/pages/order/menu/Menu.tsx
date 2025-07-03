fe63328d6663f94c0601ef999f8c1e77f51e0f81
OthmaneB-Pro <othmane93.baar@gmail.com>
2025-07-03 11:39:45 +0200

    ✨ F05 : order > menu > create first fake card burger

--- src/components/pages/order/menu/Menu.tsx
+++ src/components/pages/order/menu/Menu.tsx
@@ -2,14 +2,25 @@ import styled from "styled-components";
 import { theme } from "../../../../theme";
 
 export default function Menu() {
-  return <MenuStyled>
-    <div>Hello</div>
-    <div>Hello</div>
-    <div>Hello</div>
-    <div>Hello</div>
-    <div>Hello</div>
-    <div>Hello</div>
-  </MenuStyled>;
+  return (
+    <MenuStyled>
+      <div className="card">
+        <img src="/img/burger-bacon-egg.png" alt="burger-bacon-egg" />
+        <div className="text">
+          <h2>BURGER SMOKE BBQ</h2>
+          <div className="price-button">
+            <p>5,60€</p>
+            <button>Ajouter</button>
+          </div>
+        </div>
+      </div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+    </MenuStyled>
+  );
 }
 
 const MenuStyled = styled.div`
@@ -23,4 +34,63 @@ const MenuStyled = styled.div`
   justify-items: center;
   align-items: center;
   grid-template-columns: repeat(4, 1fr);
+
+  .card {
+    width: 240px;
+    height: 330px;
+    border-radius: 15px;
+    background-color: ${theme.colors.white};
+    box-shadow: -8px 8px 20px 0px #00000033;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .text {
+      width: 190px;
+      height: 105px;
+      display: flex;
+      flex-direction: column;
+      position: relative;
+
+      padding: 0px 5px 5px 5px;
+
+      h2 {
+        font-family: "Amatic SC";
+        font-weight: ${theme.weights.bold};
+        font-size: 36px;
+        line-height: 100%;
+        color: ${theme.colors.dark};
+        height: 46px;
+      }
+
+      .price-button {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 66px;
+        position: relative;
+        bottom: 20px;
+
+        button {
+          width: 95px;
+          height: 38px;
+          border-radius: 5px;
+          border: 1px solid #ff9f1b;
+          background-color: ${theme.colors.primary_burger};
+          color: white;
+          cursor: pointer;
+        }
+      }
+    }
+
+    img {
+      width: 200px;
+      height: 145px;
+      object-fit: contain;
+      position: relative;
+      top: 10px;
+    }
+  }
 `;
