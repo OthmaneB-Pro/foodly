a7caa920647fb10be49a01badddd724fcacdf7f0
OthmaneB-Pro <othmane93.baar@gmail.com>
2025-07-06 18:05:20 +0200

    ♻️ F07 : menu > CardContainer > refacto CardContainer in Menu

---
+++ src/components/pages/order/menu/CardContainer.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
+import Card from "../../../reusable-ui/Card";
+import { formatPrice } from "../../../../utils/maths";
+import styled from "styled-components";
+
+export default function CardContainer() {
+  const [menu, setMenu] = useState(fakeMenu2);
+
+  return (
+    <CardContainerStyled>
+      {menu.length > 0
+        ? menu.map((fakeData) => (
+            <Card
+              key={fakeData.id}
+              src={fakeData.imageSource}
+              alt={fakeData.title}
+              title={fakeData.title}
+              price={formatPrice(fakeData.price)}
+            />
+          ))
+        : "C'est vide"}
+    </CardContainerStyled>
+  );
+}
+
+const CardContainerStyled = styled.div`
+  padding: 50px 92.5px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  column-gap: 85px;
+  row-gap: 60px;
+`;
