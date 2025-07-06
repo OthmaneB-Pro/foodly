0f79cf836c168bfa73894d957a686a346ae3340b
OthmaneB-Pro <othmane93.baar@gmail.com>
2025-07-06 18:32:57 +0200

    ✨ F07 : adminPanel > creation of the button algorithm

--- src/components/pages/order/menu/AdminPanel.tsx
+++ src/components/pages/order/menu/AdminPanel.tsx
@@ -1,20 +1,44 @@
 import { AiOutlinePlus } from "react-icons/ai";
-import { FiChevronUp } from "react-icons/fi";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
 import { MdModeEditOutline } from "react-icons/md";
 import styled from "styled-components";
 import { theme } from "../../../../theme";
+import { useState } from "react";
 
 export default function AdminPanel() {
+  const [isButtonOpen, setIsButtonOpen] = useState(false);
+  const [isButton2Open, setIsButton2Open] = useState(false);
+  const [isButton3Open, setIsButton3Open] = useState(false);
+
+  const handleClick = () => {
+    setIsButton2Open(true)
+    setIsButton3Open(false)
+    setIsButtonOpen(true)
+  }
+  const handleClick2 = () => {
+    setIsButton3Open(true)
+    setIsButton2Open(false)
+    setIsButtonOpen(true)
+  }
   return (
     <AdminPanelStyled>
-      <button className="first">
-        <FiChevronUp />
+      <button
+        className={isButtonOpen ? "white" : "dark"}
+        onClick={() => setIsButtonOpen(!isButtonOpen)}
+      >
+        {isButtonOpen ? <FiChevronDown /> : <FiChevronUp />}
       </button>
-      <button>
+      <button
+        onClick={handleClick}
+        className={isButton2Open ? "dark" : "white"}
+      >
         <AiOutlinePlus />
         <span>Ajouter un produit</span>
       </button>
-      <button>
+      <button
+        onClick={handleClick2}
+        className={isButton3Open ? "dark" : "white"}
+      >
         <MdModeEditOutline /> <span>Modifier un produit</span>
       </button>
     </AdminPanelStyled>
@@ -28,17 +52,21 @@ const AdminPanelStyled = styled.div`
   display: flex;
   gap: 1px;
 
-  .first {
+  
+  .white {
+    background: ${theme.colors.white};
+    color: ${theme.colors.greySemiDark};
+    border: 1px solid ${theme.colors.greyLight};
+  }
+
+  .dark {
     background: ${theme.colors.background_dark};
     color: ${theme.colors.white};
+    border: 1px solid ${theme.colors.background_dark};
   }
   button {
     height: 43px;
     border-radius: 5px 5px 0px 0px;
-    background: ${theme.colors.white};
-    color: ${theme.colors.greySemiDark};
-    border-color: ${theme.colors.greyLight};
-    border: 1px solid ${theme.colors.greyLight};
     border-bottom: none;
     padding: 13px 22px 13px 22px;
     display: flex;
