a8946909a41af1ef963d941468dff0b2603f5b95
OthmaneB-Pro <othmane93.baar@gmail.com>
2025-07-07 18:14:00 +0200

    ✨ F08 : MenuContext > add menu and setMenu to context

--- src/components/pages/order/OrderPage.tsx
+++ src/components/pages/order/OrderPage.tsx
@@ -5,17 +5,21 @@ import Navbar from "./navbar/Navbar";
 import Menu from "./menu/Menu";
 import { useState } from "react";
 import { MenuContext } from "../../../context/MenuContext";
+import { fakeMenu2 } from "../../../fakeData/fakeMenu";
 
 export default function OrderPage() {
   const { username } = useParams();
   const [isAdmin, setIsAdmin] = useState(false);
   const [mode, setMode] = useState<"add" | "edit">("add");
+  const [menu, setMenu] = useState(fakeMenu2)
 
   const MenuContextValue = {
     isAdmin,
     setIsAdmin,
     mode,
     setMode,
+    menu,
+    setMenu,
   };
 
   return (
--- src/context/MenuContext.ts
+++ src/context/MenuContext.ts
@@ -1,10 +1,13 @@
 import { createContext } from "react";
+import type { MenuType } from "../fakeData/menuType";
 
 type MenuContextType = {
   isAdmin: boolean;
   setIsAdmin: React.Dispatch<React.SetStateAction<boolean>>;
   mode: "add" | "edit";
   setMode: React.Dispatch<React.SetStateAction<"add" | "edit">>;
+  menu : MenuType[];
+  setMenu : React.Dispatch<React.SetStateAction<MenuType[]>>;
 };
 
 export const MenuContext = createContext<MenuContextType>({
@@ -12,4 +15,6 @@ export const MenuContext = createContext<MenuContextType>({
   setIsAdmin: () => {},
   mode: "add",
   setMode: () => {},
+  menu: [],
+  setMenu: () => {},
 });
