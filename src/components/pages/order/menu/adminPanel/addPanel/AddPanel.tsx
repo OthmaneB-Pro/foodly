4b9d7413cb460f0eb73cb6ac7880d571eec08c17
OthmaneB-Pro <othmane93.baar@gmail.com>
2025-07-08 19:58:00 +0200

    💄 F08 : AddPanel > final style

--- src/components/pages/order/menu/adminPanel/addPanel/AddPanel.tsx
+++ src/components/pages/order/menu/adminPanel/addPanel/AddPanel.tsx
@@ -57,5 +57,80 @@ export default function AddPanel() {
 }
 
 const AddPanelStyled = styled.form`
+  display: flex;
+  gap: 20px;
+  position: absolute;
+  left: 70px;
+
+  .input {
+    background: red;
+    padding: 8px 16px;
+    width: 613px;
+    background: #f5f5f7;
+
+    input {
+      width: 570px;
+      background: #f5f5f7;
+    }
+  }
+  .right-input {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  }
+
+  .button-text{
+    display: flex;
+    align-items: center;
+    height: 35px;
+    gap: 5px;
+    margin-top: 8px;
+
+    p{
+        color: #60bd4f;
+        margin-left: 10px;
+        display: flex;
+        align-items: center;
+
+        svg{
+            border: 1px solid #60bd4f;
+            border-radius: 100%;
+            width: 18px;
+            height: 18px;
+            margin-right: 5px;
+        }
+    }
+
+    button {
+    width: 275px;
+    height: 35px;
+    background: #60bd4f;
+    border-color: #60bd4f;
+    font-size: 12px;
+    margin: 0px;
+
+    &:hover {
+      color: #60bd4f;
+      background-color: white;
+    }
+    &:active {
+      background-color: #60bd4f;
+      color: white;
+    }
+  }
+  }
   
+
+  .image-container {
+    width: 215px;
+    height: 120px;
+    border: 1px solid red;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    img {
+      width: 100px;
+    }
+  }
 `;
