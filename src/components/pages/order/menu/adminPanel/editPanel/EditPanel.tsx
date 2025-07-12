06834ef3f90d0beccdc2f268eb0ba47cc89d224f
OthmaneB-Pro <othmane93.baar@gmail.com>
2025-07-12 13:02:31 +0200

    ✨ F09 : EditPanel > add text content + style

--- src/components/pages/order/menu/adminPanel/editPanel/EditPanel.tsx
+++ src/components/pages/order/menu/adminPanel/editPanel/EditPanel.tsx
@@ -1,5 +1,28 @@
+import { HiCursorClick } from "react-icons/hi";
+import styled from "styled-components";
+
 export default function EditPanel() {
   return (
-    <div>Modifier un produit</div>
-  )
+    <EditPanelStyled>
+      <p>
+        Cliquer sur un produit pour le modifier <HiCursorClick />
+      </p>
+    </EditPanelStyled>
+  );
 }
+
+const EditPanelStyled = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  top: 110px;
+  left: 70px;
+
+  p {
+    font-family: Amatic SC;
+    font-weight: 400;
+    font-style: Regular;
+    font-size: 24px;
+    color: #747b91;
+  }
+`;
