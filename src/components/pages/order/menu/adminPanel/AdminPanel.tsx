99be9802061daa7aa4e36a7441034cff34a3f5de
OthmaneB-Pro <othmane93.baar@gmail.com>
2025-08-01 11:32:15 +0200

    💥 F14 : AdminPanel : animation doesn't work

--- src/components/pages/order/menu/adminPanel/AdminPanel.tsx
+++ src/components/pages/order/menu/adminPanel/AdminPanel.tsx
@@ -4,20 +4,28 @@ import { useContext } from "react";
 import { MenuContext } from "../../../../../context/MenuContext";
 import AddPanel from "./addPanel/AddPanel";
 import EditPanel from "./editPanel/EditPanel";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 export default function AdminPanel() {
   const { mode, isPanelOpen, setIsPanelOpen } = useContext(MenuContext);
 
   return (
-    <AdminPanelStyled>
-      <ListPanel isPanelOpen={isPanelOpen} setIsPanelOpen={setIsPanelOpen} />
+    <TransitionGroup component={AdminPanelStyled}>
+      <CSSTransition classNames={"animation-panel"} timeout={5000}>
+        <div>
+          <ListPanel
+            isPanelOpen={isPanelOpen}
+            setIsPanelOpen={setIsPanelOpen}
+          />
 
-      {isPanelOpen && (
-        <div className="bloc">
-          {mode === "add" ? <AddPanel /> : <EditPanel />}
+          {isPanelOpen && (
+            <div className="bloc">
+              {mode === "add" ? <AddPanel /> : <EditPanel />}
+            </div>
+          )}
         </div>
-      )}
-    </AdminPanelStyled>
+      </CSSTransition>
+    </TransitionGroup>
   );
 }
 
@@ -25,6 +33,28 @@ const AdminPanelStyled = styled.div`
   position: sticky;
   bottom: 0;
   margin-left: 350px;
+
+  .animation-panel-enter {
+    opacity: 0;
+    transform: translateY(200px);
+  }
+
+  .animation-panel-enter-active {
+    opacity: 1;
+    transform: translateY(0);
+    transition: all 0.5s ease;
+  }
+
+  .animation-panel-exit {
+    opacity: 1;
+    transform: translateY(0);
+  }
+
+  .animation-panel-exit-active {
+    opacity: 0;
+    transform: translateY(200px);
+    transition: all 0.5s ease;
+  }
 
   .bloc {
     height: 210px;
