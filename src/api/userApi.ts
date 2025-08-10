f43eea3ddb018f1606798d046cb4c90d1fe03564
OthmaneB-Pro <othmane.baar@gmail.com>
2025-08-10 07:03:25 +0200

    ✨ Api > put token on localStorage and new const for logout

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,28 +1,35 @@
 import axios from "axios";
 
 type UserType = {
-    username : string;
-    password : string;
-}
+  username: string;
+  password: string;
+};
 
-export const registerUser = async (userData : UserType) => {
-    try{
-        const res = await axios.post('http://localhost:8080/auth/register', userData)
-        return res.data;
-    }
-    catch(err){
-        console.error("Erreur lors de l'inscription", err);
-        throw err;
-    }
-}
+export const registerUser = async (userData: UserType) => {
+  try {
+    const res = await axios.post(
+      "http://localhost:8080/auth/register",
+      userData
+    );
+    const token = res.data.token;
+    localStorage.setItem("jwt", token);
+  } catch (err) {
+    console.error("Erreur lors de l'inscription", err);
+    throw err;
+  }
+};
 
-export const loginUser = async (userData : UserType) => {
-    try{
-        const res = await axios.post('http://localhost:8080/auth/login', userData)
-        return res.data;
-    }
-    catch(err){
-        console.error("Erreur lors de la connexion", err);
-        throw err;
-    }
-}
+export const loginUser = async (userData: UserType) => {
+  try {
+    const res = await axios.post("http://localhost:8080/auth/login", userData);
+    const token = res.data.token;
+    localStorage.setItem("jwt", token);
+  } catch (err) {
+    console.error("Erreur lors de la connexion", err);
+    throw err;
+  }
+};
+
+export const logout = () => {
+  localStorage.removeItem("jwt");
+};
