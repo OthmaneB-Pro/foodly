b5c000af9967680f315dd7b1a2cc1c1f504bf372
OthmaneB-Pro <othmane.baar@gmail.com>
2025-08-16 18:24:36 +0200

    ✨ test user event

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,6 +1,7 @@
-
+import {screen} from '@testing-library/react'
 
 test('first test exemple', () => { 
     expect(1 + 1).toBe(2)
-    
+    const buttonElement = screen.getByRole('button')
+    expect(buttonElement).toBeInTheDocument()
  })
