581a56f186401793a61915456bd2ca7fe24237ba
OthmaneB-Pro <othmane.baar@gmail.com>
2025-08-17 07:36:37 +0200

    ✅ Test > passing ErroPage Test with vi.mock

--- src/tests/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import {screen} from '@testing-library/react'
-
-test('first test exemple', () => { 
-    expect(1 + 1).toBe(2)
-    const buttonElement = screen.getByRole('button')
-    expect(buttonElement).toBeInTheDocument()
- })
